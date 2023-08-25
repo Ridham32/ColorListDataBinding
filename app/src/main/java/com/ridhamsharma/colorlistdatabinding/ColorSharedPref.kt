8b4d6dba16f249f9cfac9fb21c1267e13996b811
Ridham32 <sharmaridham473@gmail.com>
2023-08-25 22:05:25 +0530

    Initial commit

---
+++ app/src/main/java/com/ridhamsharma/colorlistdatabinding/ColorSharedPref.kt
@@ -0,0 +1,11 @@
+package com.ridhamsharma.colorlistdatabinding
+
+import android.content.SharedPreferences
+
+class ColorSharedPref {
+     var sharedPref: SharedPreferences?= null
+    var editor: SharedPreferences.Editor?= null
+
+
+
+}
